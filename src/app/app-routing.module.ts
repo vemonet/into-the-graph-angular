204ee86482fe0db72b29463a0295dd846206ee9f
vemonet <vincent.emonet@gmail.com>
2019-05-04 04:19:53 +0200

    add routing to DatasetsOverviewComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DatasetsOverviewComponent } from './datasets-overview/datasets-overview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DatasetsOverviewComponent}
+  //{ path: 'about', component: AboutComponent },
+  // For params: { path: 'contact/:id', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
