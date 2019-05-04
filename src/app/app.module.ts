f42817aef9a6d4b4642001993c1ace15d2d391c6
vemonet <vincent.emonet@gmail.com>
2019-05-04 03:50:03 +0200

    add module import for material design (and material icon)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Deactivate animations: import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import {MatButtonModule, MatCheckboxModule, MatIcon} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule, MatIcon,
     AppRoutingModule
   ],
   providers: [],
