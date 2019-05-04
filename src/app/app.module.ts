d5814b006da61f2ea3ca85f97e9a2f8bcf1f905d
vemonet <vincent.emonet@gmail.com>
2019-05-04 03:58:22 +0200

    fix material components import and display a mat button on index. it works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // Deactivate animations: import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatCheckboxModule, MatIcon} from '@angular/material';
+import { MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatCheckboxModule} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +19,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule, MatCheckboxModule, MatIcon,
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatCheckboxModule,
     AppRoutingModule
   ],
   providers: [],
