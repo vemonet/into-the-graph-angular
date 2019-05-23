7af38a128f01378de3f0e835bf2be6e29332f424
vemonet <vincent.emonet@gmail.com>
2019-05-23 17:58:34 +0200

    now the whole website work properly with the new navbar

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { DatasetsInfoService } from '../../datasets-info.service';
 
 @Component({
   selector: 'app-main-nav',
@@ -9,12 +10,22 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./main-nav.component.css']
 })
 export class MainNavComponent {
+  title = 'rdf-graph-explorer';
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver,
+              private datasetsInfo: DatasetsInfoService) {}
 
+  applyFilterNav(filterValue: string) {
+    this.datasetsInfo.arrayDatasetsNav = Object.keys(this.datasetsInfo.hashAll);
+
+    this.datasetsInfo.arrayDatasetsNav = this.datasetsInfo.arrayDatasetsNav
+      .filter(item => item.includes(filterValue.trim().toLowerCase()));
+    console.log('after filter');
+    console.log(this.datasetsInfo.arrayDatasetsNav);
+  }
 }
