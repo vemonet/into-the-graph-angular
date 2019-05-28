e85f71c6bd2eec4fdca716db6414c0b9f8fdc181
vemonet <vincent.emonet@gmail.com>
2019-05-28 14:48:47 +0200

    remove useless showgraph class

--- src/app/dataset-details/dataset-details.component.ts
+++ src/app/dataset-details/dataset-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, AfterViewInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MatSort } from '@angular/material';
 
@@ -29,14 +29,11 @@ export class DatasetDetailsComponent implements OnInit {
           {data: {source: 'j', target: 'e', faveColor: '#6FB1FC'}},
           {data: {source: 'j', target: 'k', faveColor: '#6FB1FC'}},
           {data: {source: 'j', target: 'g', faveColor: '#6FB1FC'}},
-
           {data: {source: 'e', target: 'j', faveColor: '#EDA1ED'}},
           {data: {source: 'e', target: 'k', faveColor: '#EDA1ED'}},
-
           {data: {source: 'k', target: 'j', faveColor: '#86B342'}},
           {data: {source: 'k', target: 'e', faveColor: '#86B342'}},
           {data: {source: 'k', target: 'g', faveColor: '#86B342'}},
-
           {data: {source: 'g', target: 'j', faveColor: '#F5A45D'}}
       ]
   };
@@ -55,14 +52,13 @@ export class DatasetDetailsComponent implements OnInit {
     { id: 'class2', value: 'Object class' },
     { id: 'classCount2', value: '# of instance of object' }];
 
-  //ngAfterViewInit() {
   ngOnInit() {
     this.displayedColumns = this.columnNames.map(x => x.id);
     console.log('before ngOnInit dataset-detail. getNavigatedData:');
+    // Trying to get data through the router to avoid doing useless calls:
     // this.route
     //   .data
     //   .subscribe(v => this.datasetsInfo = v.datasetsInfo);
-    
     // const navigatedData = this.router.getNavigatedData();
     // console.log(navigatedData);
     // if (navigatedData == null) {
@@ -82,69 +78,6 @@ export class DatasetDetailsComponent implements OnInit {
     }
     console.log('after ngOnInit dataset-detail. datasetsInfo:')
     console.log(this.datasetsInfo);
-    //this.showGraph();
-  }
-
-  showGraph() {
-    // this.ngxGraph.nodes = [
-    // {
-    //   id: 'start',
-    //   label: 'scan',
-    // }, {
-    //   id: '1',
-    //   label: 'Event#a',
-    // }, {
-    //   id: '2',
-    //   label: 'Event#x',
-    // }, {
-    //   id: '3',
-    //   label: 'Event#b',
-    // }, {
-    //   id: '4',
-    //   label: 'Event#c',
-    // }, {
-    //   id: '5',
-    //   label: 'Event#y',
-    // }, {
-    //   id: '6',
-    //   label: 'Event#z',
-    // }
-    // ];
-
-    // this.ngxGraph.links = [
-    // {
-    //   id: 'a',
-    //   source: 'start',
-    //   target: '1',
-    //   label: 'Process#1'
-    // }, {
-    //   id: 'b',
-    //   source: 'start',
-    //   target: '2',
-    //   label: 'Process#2'
-    // }, {
-    //   id: 'c',
-    //   source: '1',
-    //   target: '3',
-    //   label: 'Process#3'
-    // }, {
-    //   id: 'd',
-    //   source: '2',
-    //   target: '4',
-    //   label: 'Process#4'
-    // }, {
-    //   id: 'e',
-    //   source: '2',
-    //   target: '6',
-    //   label: 'Process#6'
-    // }, {
-    //   id: 'f',
-    //   source: '3',
-    //   target: '5',
-    //   label: 'second link'
-    // }
-    // ];
-
   }
 
   applyFilterRelationsTable(filterValue: string) {
