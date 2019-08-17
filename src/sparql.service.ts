38bc6e624e49cf5902395e94f3a7084f346d1652
vemonet <vincent.emonet@gmail.com>
2019-08-17 17:43:25 +0200

    add URI resolver at some place. And now redictering to the app! For nice browsing in app

--- src/sparql.service.ts
+++ src/sparql.service.ts
@@ -19,6 +19,7 @@ export class SparqlService {
     rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
     rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
     dc: 'http://purl.org/dc/elements/1.1/',
+    obo: 'http://purl.obolibrary.org/obo/',
     owl: 'http://www.w3.org/2002/07/owl#'
   };
 
@@ -278,8 +279,9 @@ export class SparqlService {
   // resolve URI namespace to use a prefix and add link to full URI
   public shortenUri(uriToShorten: string) {
     if (uriToShorten.startsWith('http://') || uriToShorten.startsWith('https://')) {
-      const beginUrlToDisplay = `<a href="` + uriToShorten + `">`;
-      for (const prefix in this.prefixRegistry){
+      // TODO: make it a variable
+      const beginUrlToDisplay = `<a href="http://localhost:4200/describe?uri=` + uriToShorten + `">`;
+      for (const prefix in this.prefixRegistry) {
         if (uriToShorten.startsWith(this.prefixRegistry[prefix])) {
           return beginUrlToDisplay + uriToShorten.replace(this.prefixRegistry[prefix], prefix + ':') + '</a>';
         }
