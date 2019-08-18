06b53a6c1af4b8f28fab87b2a19738df78336a14
vemonet <vincent.emonet@gmail.com>
2019-08-18 22:32:01 +0200

    the 2 datatables in overview are working

--- src/app/datasets-overview/datasets-overview.component.ts
+++ src/app/datasets-overview/datasets-overview.component.ts
@@ -85,39 +85,39 @@ export class DatasetsOverviewComponent implements OnInit, AfterViewInit {
       } ORDER BY DESC(?statements)`)
       .set('format', 'json');
 
-    // const relationSparqlHttpParams = new HttpParams()
-    //   .set('query', `PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    //   PREFIX dct: <http://purl.org/dc/terms/>
-    //   PREFIX bl: <http://w3id.org/biolink/vocab/>
-    //   PREFIX dctypes: <http://purl.org/dc/dcmitype/>
-    //   PREFIX idot: <http://identifiers.org/idot/>
-    //   PREFIX dcat: <http://www.w3.org/ns/dcat#>
-    //   PREFIX void: <http://rdfs.org/ns/void#>
-    //   PREFIX dc: <http://purl.org/dc/elements/1.1/>
-    //   PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-    //   PREFIX void-ext: <http://ldf.fi/void-ext#>
-    //   SELECT DISTINCT ?source ?graph ?classCount1 ?class1 ?relationWith ?classCount2 ?class2
-    //   WHERE {
-    //     GRAPH ?g {
-    //       ?dataset a dctypes:Dataset ;
-    //         idot:preferredPrefix ?source .
-    //       ?version dct:isVersionOf ?dataset ; 
-    //         dcat:distribution ?rdfDistribution .
-    //       ?rdfDistribution a void:Dataset ;
-    //         dcat:accessURL ?graph .
-    //       ?rdfDistribution void:propertyPartition [
-    //           void:property ?relationWith ;
-    //           void:classPartition [
-    //               void:class ?class1 ;
-    //               void:distinctSubjects ?classCount1 ;
-    //           ];
-    //           void-ext:objectClassPartition [
-    //             void:class ?class2 ;
-    //             void:distinctObjects ?classCount2 ;
-    //       ]] .
-    //     }
-    //   } ORDER BY DESC(?classCount1)`)
-    //   .set('format', 'json');
+    const relationSparqlHttpParams = new HttpParams()
+      .set('query', `PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX dct: <http://purl.org/dc/terms/>
+      PREFIX bl: <http://w3id.org/biolink/vocab/>
+      PREFIX dctypes: <http://purl.org/dc/dcmitype/>
+      PREFIX idot: <http://identifiers.org/idot/>
+      PREFIX dcat: <http://www.w3.org/ns/dcat#>
+      PREFIX void: <http://rdfs.org/ns/void#>
+      PREFIX dc: <http://purl.org/dc/elements/1.1/>
+      PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+      PREFIX void-ext: <http://ldf.fi/void-ext#>
+      SELECT DISTINCT ?source ?graph ?classCount1 ?class1 ?relationWith ?classCount2 ?class2
+      WHERE {
+        GRAPH ?g {
+          ?dataset a dctypes:Dataset ;
+            idot:preferredPrefix ?source .
+          ?version dct:isVersionOf ?dataset ; 
+            dcat:distribution ?rdfDistribution .
+          ?rdfDistribution a void:Dataset ;
+            dcat:accessURL ?graph .
+          ?rdfDistribution void:propertyPartition [
+              void:property ?relationWith ;
+              void:classPartition [
+                  void:class ?class1 ;
+                  void:distinctSubjects ?classCount1 ;
+              ];
+              void-ext:objectClassPartition [
+                void:class ?class2 ;
+                void:distinctObjects ?classCount2 ;
+          ]] .
+        }
+      } ORDER BY DESC(?classCount1)`)
+      .set('format', 'json');
 
     // Execute SPARQL query using HTTP GET
     this.http.get(this.sparql.sparqlEndpoint, { params: datasetSparqlHttpParams, headers: httpHeaders})
@@ -133,14 +133,12 @@ export class DatasetsOverviewComponent implements OnInit, AfterViewInit {
         console.log(this.datasetStatSparqlResultArray );
 
         // Get relations for each dataset
-        // this.http.get(this.sparql.sparqlEndpoint, { params: relationSparqlHttpParams, headers: httpHeaders})
-        //   .subscribe(relationData => {
-        //     this.entitiesRelationSparqlResultArray = jQuery.extend(true, {}, relationData['results']['bindings'])
-        //     //this.entitiesRelationSparqlResultArray = relationData['results']['bindings'];
-        //     console.log('Data about entities relations retrieved:');
-        //     console.log(this.entitiesRelationSparqlResultArray);
-        //   });
-        return datasetStatSparqlResultArray;
+        this.http.get(this.sparql.sparqlEndpoint, { params: relationSparqlHttpParams, headers: httpHeaders})
+          .subscribe(relationData => {
+            this.entitiesRelationSparqlResultArray = relationData['results']['bindings'];
+            console.log('Data about entities relations retrieved:');
+            console.log(this.entitiesRelationSparqlResultArray);
+          });
       });
     }
   }
