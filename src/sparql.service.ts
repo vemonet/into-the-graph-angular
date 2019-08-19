921f847777f9b422a97688b789049809c2370078
vemonet <vincent.emonet@gmail.com>
2019-08-19 04:49:49 +0200

    comment rxjs that cause compile error

--- src/sparql.service.ts
+++ src/sparql.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
-import 'rxjs/add/operator/map';
+// import 'rxjs/add/operator/map';
 
 import { DatasetsInfoService } from './datasets-info.service';
 
