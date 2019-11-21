a6d0ff2bcb0aacf733e51bf34b657562950e3daf
Vincent Emonet <vincent.emonet@gmail.com>
2019-11-21 21:21:28 +0100

    fix some sparql queries templates

--- src/app/sparql/sparql.component.ts
+++ src/app/sparql/sparql.component.ts
@@ -14,26 +14,27 @@ export class SparqlComponent implements AfterViewInit {
 
   statisticsQuery = `PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
   PREFIX dct: <http://purl.org/dc/terms/>
-  PREFIX bl: <http://w3id.org/biolink/vocab/>
   PREFIX dctypes: <http://purl.org/dc/dcmitype/>
   PREFIX idot: <http://identifiers.org/idot/>
   PREFIX dcat: <http://www.w3.org/ns/dcat#>
   PREFIX void: <http://rdfs.org/ns/void#>
-  SELECT distinct ?source ?statements ?entities ?properties ?classes ?graph
+  
+  SELECT distinct ?graph ?statements ?entities ?properties ?classes
   WHERE {
     GRAPH ?g {
-      ?dataset a dctypes:Dataset ; idot:preferredPrefix ?source .
-      ?version dct:isVersionOf ?dataset ; dcat:distribution ?rdfDistribution .
-      ?rdfDistribution a void:Dataset ;
-        dcat:accessURL ?graph ;
+      #?dataset a dctypes:Dataset ; idot:preferredPrefix ?source .
+      #?version dct:isVersionOf ?dataset ; dcat:distribution ?rdfDistribution .
+      ?graph a void:Dataset ; 
+        #dcat:accessURL ?graph ; 
         void:triples ?statements ;
         void:entities ?entities ;
         void:properties ?properties .
+  
       ?rdfDistribution void:classPartition [
         void:class rdfs:Class ;
         void:distinctSubjects ?classes
       ] .
-    }
+    } 
   } ORDER BY DESC(?statements)`;
 
   entitiesRelationsQuery = `PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
@@ -43,18 +44,21 @@ export class SparqlComponent implements AfterViewInit {
   PREFIX dcat: <http://www.w3.org/ns/dcat#>
   PREFIX void: <http://rdfs.org/ns/void#>
   PREFIX void-ext: <http://ldf.fi/void-ext#>
-  SELECT distinct ?source ?classCount1 ?class1 ?relationWith ?classCount2 ?class2
+  
+  # Show relations between classes in the graph (and count)
+  SELECT distinct ?graph ?classCount1 ?class1 ?relationWith ?classCount2 ?class2
   WHERE {
-    GRAPH ?g {
-      ?dataset a dctypes:Dataset ; idot:preferredPrefix ?source .
-      ?version dct:isVersionOf ?dataset ; dcat:distribution ?rdfDistribution .
-      ?rdfDistribution a void:Dataset ;
-        dcat:accessURL ?graph .
-      ?rdfDistribution void:classPartition [
+    GRAPH ?metadataGraph {
+      ?graph a void:Dataset .
+        #idot:preferredPrefix ?source .
+        # Or Use dc:identifier ?
+  
+      ?graph void:classPartition [
         void:class rdfs:Class ;
         void:distinctSubjects ?classes
       ] .
-      ?rdfDistribution void:propertyPartition [
+  
+      ?graph void:propertyPartition [
           void:property ?relationWith ;
           void:classPartition [
               void:class ?class1 ;
@@ -63,8 +67,8 @@ export class SparqlComponent implements AfterViewInit {
           void-ext:objectClassPartition [
             void:class ?class2 ;
             void:distinctObjects ?classCount2 ;
-      ]] .
-    }
+      ]] . 
+    } 
   } ORDER BY ?source DESC(?classCount1) DESC(?classCount2)`;
 
   constructor() { }
