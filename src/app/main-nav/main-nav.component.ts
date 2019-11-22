a4a8eeb98e4ad3291890c9d63443d11c45e91e59
Vincent Emonet <vincent.emonet@gmail.com>
2019-11-22 16:46:22 +0100

    add search form in navbar

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { FormControl, FormGroup } from "@angular/forms";
+import { Router } from "@angular/router";
 //import { DatasetsInfoService } from '../../datasets-info.service';
 
 @Component({
@@ -11,7 +13,15 @@ import { map } from 'rxjs/operators';
 })
 export class MainNavComponent {
   title = 'into-the-graph';
+  searchForm: FormGroup;
 
-  constructor() {}
+  constructor(private router: Router) {}
+
+  onSubmit(){
+    this.router.navigateByData({
+      url: ['/search'],
+      data: { searchText: this.searchForm.get('SearchText').value }
+    });
+  }
 
 }
