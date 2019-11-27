e5e173626ab458c6957825cb70fab0dd77a11559
Vincent Emonet <vincent.emonet@gmail.com>
2019-11-27 07:59:22 +0100

    use SPARQL query to use GraphDB search index when search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -31,7 +31,7 @@ export class SearchComponent implements OnInit {
     console.log(this.searchSparqlResultArray);
   }
 
-  // TODO: use ASK?
+  // TODO: use ASK? Or better, just directly redirect to Describe component
   getDatasetData(searchText: string) {
     const searchUriSparqlHttpParams = new HttpParams()
       .set('query', `SELECT DISTINCT ?searchResult WHERE {
@@ -46,13 +46,22 @@ export class SearchComponent implements OnInit {
     } LIMIT 5`)
       .set('format', 'json');
 
+    // Generic search query, only on rdfs:label and bl:name
+    // const searchTextSparqlHttpParams = new HttpParams()
+    //   .set('query', `SELECT ?searchResult WHERE {
+    //     ?searchResult ?p ?o .
+    //     VALUES ?p {<http://www.w3.org/2000/01/rdf-schema#label> <https://w3id.org/biolink/vocab/name>} .
+    //     FILTER(isLiteral(?o))
+    //     FILTER contains(?o, "` + this.searchText + `")
+    //     } LIMIT 5`)
+    //   .set('format', 'json');
+
+    // Using GraphDB search index
     const searchTextSparqlHttpParams = new HttpParams()
-      .set('query', `SELECT ?searchResult WHERE {
-        ?searchResult ?p ?o .
-        VALUES ?p {<http://www.w3.org/2000/01/rdf-schema#label> <https://w3id.org/biolink/vocab/name>} .
-        FILTER(isLiteral(?o))
-        FILTER contains(?o, "` + this.searchText + `")
-        } LIMIT 5`)
+      .set('query', `PREFIX luc: <http://www.ontotext.com/owlim/lucene#>
+        SELECT ?searchResult {
+          ?searchResult luc:labelIndex "*` + this.searchText + `*"
+        }`)
       .set('format', 'json');
 
     if (this.searchText.startsWith('http')) {
