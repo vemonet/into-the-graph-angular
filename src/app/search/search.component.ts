623d9864a9a0a1752cf28a1e5edef7d19f206213
Vincent Emonet <vincent.emonet@gmail.com>
2019-12-02 07:21:30 +0100

    try to use environment variables to define SPARQL endpoint URL at runtime create search component and open it when search text submitted put back generic graph search instead of graphdb full text index

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -47,22 +47,22 @@ export class SearchComponent implements OnInit {
       .set('format', 'json');
 
     // Generic search query, only on rdfs:label and bl:name
-    // const searchTextSparqlHttpParams = new HttpParams()
-    //   .set('query', `SELECT ?searchResult WHERE {
-    //     ?searchResult ?p ?o .
-    //     VALUES ?p {<http://www.w3.org/2000/01/rdf-schema#label> <https://w3id.org/biolink/vocab/name>} .
-    //     FILTER(isLiteral(?o))
-    //     FILTER contains(?o, "` + this.searchText + `")
-    //     } LIMIT 5`)
-    //   .set('format', 'json');
+    const searchTextSparqlHttpParams = new HttpParams()
+      .set('query', `SELECT ?searchResult WHERE {
+        ?searchResult ?p ?o .
+        VALUES ?p {<http://www.w3.org/2000/01/rdf-schema#label> <https://w3id.org/biolink/vocab/name>} .
+        FILTER(isLiteral(?o))
+        FILTER contains(?o, "` + this.searchText + `")
+        } LIMIT 5`)
+      .set('format', 'json');
 
     // Using GraphDB search index
-    const searchTextSparqlHttpParams = new HttpParams()
-      .set('query', `PREFIX luc: <http://www.ontotext.com/owlim/lucene#>
-        SELECT ?searchResult {
-          ?searchResult luc:labelIndex "*` + this.searchText + `*"
-        }`)
-      .set('format', 'json');
+    // const searchTextSparqlHttpParams = new HttpParams()
+    //   .set('query', `PREFIX luc: <http://www.ontotext.com/owlim/lucene#>
+    //     SELECT ?searchResult {
+    //       ?searchResult luc:labelIndex "*` + this.searchText + `*"
+    //     }`)
+    //   .set('format', 'json');
 
     if (this.searchText.startsWith('http')) {
       this.http.get(this.sparql.sparqlEndpoint, { params: searchUriSparqlHttpParams, headers: this.sparql.httpHeaders})
